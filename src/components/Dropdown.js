5ac60d6feb1034c136ea7ca61c69659f55d5abbc
David Lam <davidlam.yc@gmail.com>
2020-08-14 13:26:41 +0800

    init

---
+++ src/components/Dropdown.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+const Dropdown = ({label, options, selected, onSelectedChange}) => {
+    const [open, setOpen] = useState(false);
+    // const [color, setColor] = useState('red');
+    const ref = useRef();
+
+    useEffect(() => {
+        const onBodyClick = (event) => {
+            if (ref.current.contains(event.target)) {
+                return;
+            }
+
+            setOpen(false);
+        };
+
+        document.body.addEventListener('click', onBodyClick);
+
+        return () => {
+            document.body.removeEventListener('click', onBodyClick);
+        }
+    }, [])
+
+    const renderedOptions = options.map(option => {
+        if (option.value === selected.value) {
+            return null;
+        }
+
+        return (
+            <div
+                key={option.value} 
+                className="item"
+                onClick={() => {
+                    onSelectedChange(option);
+                    // setColor(option.value);
+                }}
+            >
+                {option.label}
+            </div>
+        )
+    });
+
+    // const coloredText = () => {
+    //     return (
+    //         <div className="content" style={{color}}>
+    //             I am colored!
+    //         </div>
+    //     )
+    // }
+    
+
+    return (
+        <div ref={ref} className="ui form">
+            <div className="field">
+    <label className="label">{label}</label>
+                <div
+                    onClick={() => setOpen(!open)} 
+                    className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+                >
+                    <i className="dropdown icon"></i>
+                    <div className="text">{selected.label}</div>
+                    <div className={`menu ${open ? 'visible transition' : ''}`}>
+                        {renderedOptions}
+                    </div>
+                </div>
+                {/* {coloredText()} */}
+            </div>
+        </div>
+    )
+}
+
+export default Dropdown;
